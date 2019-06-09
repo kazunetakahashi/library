07ca7a4fc3c477fac4a85f2107309086ba7bdec6
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-06-10 03:54:59 +0900

    fixed matrix and mint

--- cpp/combi_power_gcm.cpp
+++ cpp/combi_power_gcm.cpp
@@ -1,52 +1,148 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_SIZE = 1000010;
-const long long MOD = 1000000007;
+typedef long long ll;
 
-long long inv[MAX_SIZE];
-long long fact[MAX_SIZE];
-long long factinv[MAX_SIZE];
+const int MAX_SIZE = 1000010;
+
+class mint
+{
+public:
+  static ll MOD;
+  ll x;
+
+  mint() : x(0) {}
+
+  mint(ll x) : x(x % MOD) {}
+
+  mint &fix()
+  {
+    x = (x % MOD + MOD) % MOD;
+    return *this;
+  }
+
+  mint operator-() const
+  {
+    return mint(0) - *this;
+  }
+
+  mint &operator+=(const mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+
+  mint &operator-=(const mint &a)
+  {
+    if ((x += MOD - a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+
+  mint &operator*=(const mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+
+  mint &operator/=(const mint &a)
+  {
+    return (*this *= power(MOD - 2));
+  }
+
+  mint operator+(const mint &a) const
+  {
+    return mint(*this) += a;
+  }
+
+  mint operator-(const mint &a) const
+  {
+    return mint(*this) -= a;
+  }
+
+  mint operator*(const mint &a) const
+  {
+    return mint(*this) *= a;
+  }
+
+  mint operator/(const mint &a) const
+  {
+    return mint(*this) /= a;
+  }
+
+  bool operator<(const mint &a) const
+  {
+    return x < a.x;
+  }
+
+  bool operator==(const mint &a) const
+  {
+    return x == a.x;
+  }
+
+  const mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+
+ll mint::MOD = 1e9 + 7;
+
+istream &operator>>(istream &stream, mint &a)
+{
+  stream >> a.x;
+  return stream;
+}
+
+ostream &operator<<(ostream &stream, const mint &a)
+{
+  stream << a.x;
+  return stream;
+}
+
+mint inv[MAX_SIZE];
+mint fact[MAX_SIZE];
+mint factinv[MAX_SIZE];
 
 void init()
 {
   inv[1] = 1;
   for (int i = 2; i < MAX_SIZE; i++)
   {
-    inv[i] = ((MOD - inv[MOD % i]) * (MOD / i)) % MOD;
+    inv[i] = (-inv[mint::MOD % i]) * (mint::MOD / i);
   }
   fact[0] = factinv[0] = 1;
   for (int i = 1; i < MAX_SIZE; i++)
   {
-    fact[i] = (i * fact[i - 1]) % MOD;
-    factinv[i] = (inv[i] * factinv[i - 1]) % MOD;
+    fact[i] = mint(i) * fact[i - 1];
+    factinv[i] = inv[i] * factinv[i - 1];
   }
 }
 
-long long C(int n, int k)
+mint C(int n, int k)
 {
   if (n >= 0 && k >= 0 && n - k >= 0)
   {
-    return ((fact[n] * factinv[k]) % MOD * factinv[n - k]) % MOD;
+    return fact[n] * factinv[k] * factinv[n - k];
   }
   return 0;
-}
-
-long long power(long long x, long long n)
-{
-  if (n == 0)
-  {
-    return 1;
-  }
-  else if (n % 2 == 1)
-  {
-    return (x * power(x, n - 1)) % MOD;
-  }
-  else
-  {
-    long long half = power(x, n / 2);
-    return (half * half) % MOD;
-  }
 }
 
 long long gcd(long long x, long long y)
