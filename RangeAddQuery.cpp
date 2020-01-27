d0fbc9fd13be9d3e024f10d1144c5efd18f63ea0
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-01-27 14:29:04 +0900

    RAQ

---
+++ RangeAddQuery.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+template <typename T = ll>
+class RangeAddQuery
+{ // index starts at 0.
+  int N;
+  vector<T> data;
+
+public:
+  RangeAddQuery(int N) : N{N}, data(N + 1, T{0}) {}
+
+  T get(int i)
+  {
+    return _sum(i + 1);
+  }
+
+  void add(int a, int b, T x)
+  { // [a, b)
+    _add(a + 1, x);
+    _add(b + 1, -x);
+  }
+
+private:
+  T _sum(int i)
+  { // [1, i]
+    T s{0};
+    while (i > 0)
+    {
+      s += data[i];
+      i -= i & -i;
+    }
+    return s;
+  }
+
+  void _add(int i, ll x)
+  {
+    while (i <= N)
+    {
+      data[i] += x;
+      i += i & -i;
+    }
+  }
+};
+
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_E&lang=ja
+
+int main()
+{
+  int N, Q;
+  cin >> N >> Q;
+  RangeAddQuery<ll> RAQ(N);
+  for (auto i = 0; i < Q; ++i)
+  {
+    int c;
+    cin >> c;
+    if (c == 0)
+    {
+      int s, t;
+      ll x;
+      cin >> s >> t >> x;
+      --s;
+      --t;
+      RAQ.add(s, t + 1, x);
+    }
+    else
+    {
+      int i;
+      cin >> i;
+      --i;
+      cout << RAQ.get(i) << endl;
+    }
+  }
+}
